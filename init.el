a6054d46e1a4fcda7abce34a7a58329333ba4266
brogowski <bartlomiej.rogowski@degustudios.com>
2022-04-08 23:05:02 +0200

    Add translate and spell checking

--- init.el
+++ init.el
@@ -86,6 +86,10 @@
   (setq org-todo-keywords
         '((sequence "TODO(t)" "DOING(d)" "|" "COMPLETE(c)"))))
 
+;; Turn on spell checking for org files
+(add-hook 'org-mode-hook 'turn-on-flyspell)
+(global-set-key (kbd "C-c s") 'flyspell-check-previous-highlighted-word)
+
 ;; Try to center org files content and wrap lines by words
 (defun my/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
@@ -140,6 +144,35 @@
   ("C-c n d" . org-roam-dailies-map)
   :config
   (org-roam-db-autosync-mode))
+
+;; Translator
+(use-package go-translate
+  :custom
+  (gts-translate-list '(("pl" "en"))))
+
+(defun my/go-translate ()
+  (interactive)
+  (gts-translate (gts-translator
+                  :picker (gts-prompt-picker)
+                  :engines (gts-google-engine)
+                  :render (gts-buffer-render))))
+
+(defun my/go-translate-at-point ()
+  (interactive)
+  (gts-translate (gts-translator
+                  :picker (gts-noprompt-picker)
+                  :engines (gts-google-engine)
+                  :render (gts-message-render))))
+
+;; go-translate renderer to mini-buffer
+(defclass gts-message-render (gts-render) ())
+
+(cl-defmethod gts-out ((_ gts-message-render) task)
+  (deactivate-mark)
+  (with-slots (result ecode) task
+    (if ecode
+        (user-error "%s" result)      
+      (message (nth 0 (split-string result "\n"))))))
 
 ;; Dired settings
 (use-package dired
@@ -212,7 +245,7 @@
  '(custom-safe-themes
    '("47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "a6e620c9decbea9cac46ea47541b31b3e20804a4646ca6da4cce105ee03e8d0e" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" default))
  '(package-selected-packages
-   '(dired-hide-dotfiles all-the-icons-dired visual-fill-column doom-themes which-key doom-modeline marginalia orderless vertigo vertico use-package clj-refactor tagedit rainbow-delimiters projectile ido-completing-read+ clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
+   '(go-translate google-translate dired-hide-dotfiles all-the-icons-dired visual-fill-column doom-themes which-key doom-modeline marginalia orderless vertigo vertico use-package clj-refactor tagedit rainbow-delimiters projectile ido-completing-read+ clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
